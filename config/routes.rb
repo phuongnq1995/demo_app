6beaeca3e4d821b6402a6b78abfa0732caf87d70
phuongnq1995 <phuongnq.itedu@gmail.com>
2016-11-14 13:26:28 +0700

    finish login logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root             'static_pages#home'
   get 'signup'  => 'users#new'
-
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  
+  resources :users
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
