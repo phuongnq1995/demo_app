c683e1bd1078ec72fc1f01aca7d4bc3dc78c181e
phuongnq1995 <phuongnq.itedu@gmail.com>
2016-11-16 10:47:18 +0700

    finish follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   has_many :entries, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -17,5 +25,19 @@ class User < ActiveRecord::Base
   end  
   def feed
     Entry.where("user_id = ?", id)
+  end  
+  
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root             'static_pages#home'
-  get 'signup'  => 'users#new'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   
-  resources :users
-  resources :entries          
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :entries        
+  resources :relationships,       only: [:create, :destroy]    
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
