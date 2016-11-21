51c233d81d5668854296e79b41954684155304e1
phuongnq1995 <phuongnq.itedu@gmail.com>
2016-11-21 13:37:45 +0700

    Push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+59.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+users = User.order(:created_at).take(6)
+30.times do
+  content = Faker::Lorem.sentence(3)
+  title = "New title"
+  users.each { |user| user.entries.create!(content: content,title: title) }
+end
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..10]
+followers = users[3..10]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
