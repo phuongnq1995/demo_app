fc278265d4e01afd8d7f4ef19a9246c38356ea9b
phuongnq1995 <phuongnq.itedu@gmail.com>
2016-11-21 09:49:41 +0700

    Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
                                         :following, :followers]
+  def index
+    @users = User.all
+  end
 
 
   def new
