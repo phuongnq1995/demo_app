ac5ce189616bd92c3cb941f9ba7d9db1545287e1
phuongnq1995 <phuongnq.itedu@gmail.com>
2016-11-21 13:20:15 +0700

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :admin_user,     only: :destroy  
   before_action :correct_user,   only: [:edit, :update]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
                                         :following, :followers]
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], :per_page => 10)
   end
 
 
@@ -51,6 +51,11 @@ class UsersController < ApplicationController
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end  
   private
 
     def user_params
@@ -69,4 +74,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end 
+     def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117154229) do
+ActiveRecord::Schema.define(version: 20161121061522) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -51,9 +51,10 @@ ActiveRecord::Schema.define(version: 20161117154229) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
